bffb5599cfb921a9109fdb2e777f4f6e81be3c73
Malcolm Smalls <smallsbeats@gmail.com>
2021-11-05 13:02:31 -0700

    Finished task 6

--- index.js
+++ index.js
@@ -300,9 +300,20 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  const newArtist = {
+    id: 20,
+    name: 'Malcolm Smalls',
+    years: '1990 - infinity',
+    genre: 'Web Design',
+    nationality: 'American',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+  array.push(newArtist)
+  return array;
 }
+
+
+console.log(addArtist(artists))
 
 
 
